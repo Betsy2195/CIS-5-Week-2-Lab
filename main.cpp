37bb36884b25acf9d5aa3ef8b0490139dc6f5d3f
Betsy2195 <caudelbetsy@gmail.com>
2026-09-08 01:26:33 +0000

    simplified

--- main.cpp
+++ main.cpp
@@ -3,13 +3,17 @@
 // Betsy Caudel - Week 2 
 // CIS 5 Week 02 · Program structure
 
+//Removes need to rewrite "std::cout" and instead simply write cout
+using std::cout;
+
 int main() {
-  // Lines 8-12 print information in the terminal.
-  std::cout << "Name: Betsy\n";
-  std::cout << "Interest: Computer Engineering\n";
-  std::cout << "CIS 5 goal: Learn and use a new programming language effectively.\n";
-  std::cout << "Tool path: Github\n";
-  std::cout << "Fun fact: I love musicals.\n";
-  //Line 14 ends the program. Nothing runs after that.
+  // Lines 10-15 print information in the terminal.
+  cout << "Name: Betsy\n";
+  cout << "Interest: Computer Engineering\n";
+  cout << "CIS 5 goal: Learn and use a new programming language effectively.\n";
+  cout << "Tool path: Github\n";
+  cout << "Fun fact: I love musicals.\n";
+
+  //Line 18 ends the program. Nothing runs after that.
   return 0;
 }
